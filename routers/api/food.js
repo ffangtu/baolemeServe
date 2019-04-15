0543fc7c44bf8c0a2d56b299404d1aa902875e8f
ZK <762845835@qq.com>
2019-04-15 22:42:26 +0800

    add

--- routers/api/food.js
+++ routers/api/food.js
@@ -3,59 +3,84 @@ const Router = express.Router();
 const Food = require('../../MongoDBModules/food');
 
 Router.post('/save', (req, res) => {
-    Food.findOne({FoodNumber: req.body.FoodNumber})
+
+    Food.findOne({_id: req.body._id})
         .then(food => {
-                const newFood = new Food({
+            if (food && food._id !== 'null') {
+                Food.updateOne({_id: req.body._id}, {
+                    FoodImg: req.body.FoodImg,
                     FoodName: req.body.FoodName,
-                    FoodNumber: req.body.FoodNumber.toString(),
+                    FoodNumber: req.body.FoodNumber,
+                    FoodPrice: req.body.FoodPrice,
+                    FoodType: req.body.FoodType
+                }).then(re => {
+                    res.json({message: 'success'})
+                }).catch(err => {
+                    res.json({message: 'error'})
+                })
+            } else {
+                const newFood = Food({
+                    FoodImg: req.body.FoodImg,
+                    FoodName: req.body.FoodName,
+                    FoodNumber: req.body.FoodNumber,
                     FoodPrice: req.body.FoodPrice,
                     FoodType: req.body.FoodType,
-                    desc: req.body.desc,
-                    FoodImg: req.body.FoodImg
+                    desc: req.body.desc
                 });
-                newFood.save()
-                    .then(re => {
-                        res.json({message: 'success'})
-                    })
-                    .catch(err => {
-                        res.json({message: 'error'});
-                        console.log(err)
-                    })
+                newFood.save().then(re => {
+                    console.log(re)
+                    res.json({message: 'success'})
+                }).catch(err => {
+                    console.log(err)
+                    res.json({message: 'error'})
+                })
+            }
         })
+
+
 });
 
-Router.post('/getFood',(req,res)=>{
-    let ip = req.headers['x-real-ip'] ? req.headers['x-real-ip'] : req.ip.replace(/::ffff:/, '');
-    if (req.body.FoodType){
-        Food.find({FoodType:req.body.FoodType})
-            .then(food=>{
-                food.map(item=>{
-                    item.FoodImg=`${item.FoodImg}` ;
+Router.post('/getFood', (req, res) => {
+    if (req.body.FoodType) {
+        Food.find({FoodType: req.body.FoodType})
+            .then(food => {
+                food.map(item => {
+                    item.FoodImg = `${item.FoodImg}`;
                     return {
                         ...item
                     }
                 });
                 res.json(food)
             })
-            .catch(err=>{
+            .catch(err => {
                 console.log(err)
             })
     } else {
         Food.find({})
-            .then(food=>{
-                food.map(item=>{
-                   item.FoodImg=`${item.FoodImg}` ;
-                   return {
-                       ...item
-                   }
+            .then(food => {
+                food.map(item => {
+                    item.FoodImg = `${item.FoodImg}`;
+                    return {
+                        ...item
+                    }
                 });
                 res.json(food)
             })
-            .catch(err=>{
+            .catch(err => {
                 console.log(err)
             })
     }
 
+});
+
+Router.post('/remove', (req, res) => {
+    Food.remove({_id: req.body._id})
+        .then(re => {
+            res.json({message: 'success'})
+        })
+        .catch(err => {
+            res.json({message: 'error'})
+        })
 });
 
 module.exports = Router;
